f54580402425bdc02bcab2e060e20dca035cae19
Yoshida Kannyuu <e125747@w119-137.dyn.u-ryukyu.ac.jp>
2013-11-14 11:47:01 +0900

    じゃんけんだけ

---
+++ Janken.ver02/Janken.ver02/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Janken.ver02
+//
+//  Created by Yoshida Kannyuu on 2013/11/13.
+//  Copyright (c) 2013年 Yoshida Kannyuu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
