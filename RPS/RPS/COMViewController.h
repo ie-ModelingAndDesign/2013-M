804d1be487ca6dca0a1da234af471ebcd443fcd2
e125719 <e125719@e125719-no-MacBook-Pro.local>
2013-12-07 18:30:03 +0900

    add dekita

--- RPS/RPS/COMViewController.h
+++ RPS/RPS/COMViewController.h
@@ -9,5 +9,17 @@
 #import <UIKit/UIKit.h>
 
 @interface COMViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UILabel *label_msg;
+@property (weak, nonatomic) IBOutlet UILabel *label_ememy;
+@property (weak, nonatomic) IBOutlet UILabel *label_result;
 
+@property (weak, nonatomic) IBOutlet UIButton *button_rock;
+@property (weak, nonatomic) IBOutlet UIButton *button_scissors;
+@property (weak, nonatomic) IBOutlet UIButton *button_paper;
+@property (weak, nonatomic) IBOutlet UIButton *button_again;
+
+- (IBAction)push_rock:(id)sender;
+- (IBAction)push_scissors:(id)sender;
+- (IBAction)push_paper:(id)sender;
+- (IBAction)push_again:(id)sender;
 @end
