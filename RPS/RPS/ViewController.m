0baf4b9a37fc426499631c944c4836ef03beadc8
e125719 <e125719@e125719-no-MacBook-Pro.local>
2013-12-12 11:48:32 +0900

    fuga

--- RPS/RPS/ViewController.m
+++ RPS/RPS/ViewController.m
@@ -7,7 +7,7 @@
 //
 
 #import "ViewController.h"
-#import "COMViewController.h"
+//#import "COMViewController.h"
 @interface ViewController ()
 
 @end
--- RPS/RPS/commViewController.m
+++ RPS/RPS/commViewController.m
@@ -1,25 +1,18 @@
 //
-//  COMViewController.m
+//  commViewController.m
 //  RPS
 //
-//  Created by e125719 on 2013/12/05.
+//  Created by e125719 on 2013/12/11.
 //  Copyright (c) 2013年 e125719. All rights reserved.
 //
 
-#import "COMViewController.h"
+#import "commViewController.h"
 
-@interface COMViewController ()
+@interface commViewController ()
 
 @end
 
-@implementation COMViewController
-@synthesize label_ememy;
-@synthesize label_msg;
-@synthesize label_result;
-@synthesize button_again;
-@synthesize button_paper;
-@synthesize button_rock;
-@synthesize button_scissors;
+@implementation commViewController
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
@@ -42,15 +35,4 @@
     // Dispose of any resources that can be recreated.
 }
 
-- (IBAction)push_rock:(id)sender {
-}
-
-- (IBAction)push_scissors:(id)sender {
-}
-
-- (IBAction)push_paper:(id)sender {
-}
-
-- (IBAction)push_again:(id)sender {
-}
 @end
