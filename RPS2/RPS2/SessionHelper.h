734d46399528f66986085217f080f2ce1f9a3568
Yoshida Kannyuu <e125747@Kannyuu-no-MacBook-Pro.local>
2013-12-18 18:28:01 +0900

    returnでキーボード隠せた

---
+++ RPS2/RPS2/SessionHelper.h
@@ -0,0 +1,13 @@
+//
+//  SessionHelper.h
+//  RPS2
+//
+//  Created by Yoshida Kannyuu on 2013/12/18.
+//  Copyright (c) 2013年 e125719. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SessionHelper : UITableViewController
+
+@end
