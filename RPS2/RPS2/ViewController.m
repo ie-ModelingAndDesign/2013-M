b8b815bfadb1ef3daf7d2ea3b1fde563dd5b598b
Yoshida Kannyuu <e125747@Kannyuu-no-MacBook-Pro.local>
2013-12-19 11:54:37 +0900

    検索のボタンを付けてpeerBrowsにつなげてみた

--- RPS2/RPS2/ViewController.m
+++ RPS2/RPS2/ViewController.m
@@ -7,17 +7,25 @@
 //
 
 #import "ViewController.h"
+#import "PeerListViewController.h"
 
-@interface ViewController ()
+static NSString * const SegueIdentifierPushPeerListView = @"PushPeerListViewSegue";
+
+@interface ViewController () <UITextFieldDelegate>
+
+@property (weak, nonatomic) IBOutlet UITextField *displayNameTextField;
+
+- (IBAction)createSessionButtonDidTouch:(id)sender;
 
 @end
 
 @implementation ViewController
 
+#pragma mark - Lifecycle methods
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
 }
 
 - (void)didReceiveMemoryWarning
@@ -26,5 +34,33 @@
     // Dispose of any resources that can be recreated.
 }
 
+#pragma mark - Navigation methods
+
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    if ([segue.identifier isEqualToString:SegueIdentifierPushPeerListView]) {
+        PeerListViewController *viewController = segue.destinationViewController;
+        [viewController createSessionWithDisplayName:self.displayNameTextField.text];
+    }
+}
+
+#pragma mark - UITextFieldDelegate
+
+- (BOOL)textFieldShouldReturn:(UITextField *)textField
+{
+    [textField resignFirstResponder];
+    return NO;
+}
+
+#pragma mark - Handler methods
+
+- (IBAction)createSessionButtonDidTouch:(id)sender
+{
+    if (self.displayNameTextField.text.length == 0) {
+        return;
+    }
+    
+    [self performSegueWithIdentifier:SegueIdentifierPushPeerListView sender:self];
+}
 
 @end
