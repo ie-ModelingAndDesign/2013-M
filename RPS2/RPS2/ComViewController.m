d6ac6c2e4e5074f86e101264c438e71af4bcc0ed
e125719 <e125719@e125719-no-MacBook-Pro.local>
2013-12-19 11:51:08 +0900

    コメントを追記

--- RPS2/RPS2/ComViewController.m
+++ RPS2/RPS2/ComViewController.m
@@ -17,8 +17,7 @@
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
-    if (self) {
-        // Custom initialization
+    if (self) {        
     }
     return self;
 }
@@ -26,107 +25,150 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view.
+    
+    // ラベルに何も表示しない
     self.label_enemy.text = @"";
     self.label_result.text = @"";
     
+    // "もう一度"ボタンを隠す
     self.button_again.hidden = YES;
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
 
+// プレイヤーが"グー"ボタンを選択した場合
 - (IBAction)rock_push:(id)sender {
+
+    // メッセージラベルを表示
     self.label_msg.text = @"ぽん!";
     
+    // "チョキ", "パー"ボタンを隠す
     self.button_scissors.hidden = YES;
     self.button_paper.hidden = YES;
     
+    // COMのジャンケンの手を決定
     srand(time(nil));
     NSInteger janken;
     janken = rand() % 3;
     
     if (janken == 0) {
-        self.label_enemy.text = @"グー";
-        self.label_result.text = @"あいこで・・・";
+
+        // COMが"グー"を選択した場合
+        self.label_enemy.text = @"グー";  // COMが"グー"を出したと表示
+        self.label_result.text = @"あいこで・・・";  // じゃんけん結果を表示
         
+        // "チョキ", "パー"ボタンを表示
         self.button_scissors.hidden = NO;
         self.button_paper.hidden = NO;
+        
     } else if (janken == 1) {
-        self.label_enemy.text = @"チョキ";
-        self.label_result.text = @"あなたの勝ち";
         
-        self.button_rock.enabled = NO;
-        self.button_again.hidden = NO;
+        // COMが"チョキ"を選択した場合
+        self.label_enemy.text = @"チョキ";  // COMが"チョキ"を出したと表示
+        self.label_result.text = @"あなたの勝ち";  // ジャンケン結果を表示
+        
+        self.button_rock.enabled = NO;  // "グー"ボタンを使用不可に
+        self.button_again.hidden = NO;  // "もう一度"ボタンを表示
+
     } else if (janken == 2) {
-        self.label_enemy.text = @"パー";
-        self.label_result.text = @"あなたの負け";
         
-        self.button_rock.enabled = NO;
-        self.button_again.hidden = NO;
+        // COMが"パー"を選択した場合
+        self.label_enemy.text = @"パー";  // COMが"パー"を出したと表示
+        self.label_result.text = @"あなたの負け";  // ジャンケン結果を表示
+        
+        self.button_rock.enabled = NO;  // "グー"ボタンを使用不可に
+        self.button_again.hidden = NO;  // "もう一度"ボタンを表示
     }
 }
 
+// プレイヤーが"チョキ"ボタンを選択した場合
 - (IBAction)scissors_push:(id)sender {
+
+    // メッセージラベルを表示
     self.label_msg.text = @"ぽん!";
     
+    // "グー", "パー"ボタンを隠す
     self.button_rock.hidden = YES;
     self.button_paper.hidden = YES;
     
+    // COMのジャンケンの手を決定
     srand(time(nil));
     NSInteger janken;
     janken = rand() % 3;
     
     if (janken == 0) {
-        self.label_enemy.text = @"グー";
-        self.label_result.text = @"あなたの負け";
         
-        self.button_scissors.enabled = NO;
-        self.button_again.hidden = NO;
+        // COMが"グー"を選択した場合
+        self.label_enemy.text = @"グー";  // COMが"グー"を出したと表示
+        self.label_result.text = @"あなたの負け";  // ジャンケン結果を表示
+        
+        self.button_scissors.enabled = NO;  // "チョキ"ボタンを使用不可に
+        self.button_again.hidden = NO;  // "もう一度"ボタンを表示
+
     } else if (janken == 1) {
-        self.label_enemy.text = @"チョキ";
-        self.label_result.text = @"あいこで・・・";
         
+        // COMが"チョキ"を選択した場合
+        self.label_enemy.text = @"チョキ";  // COMが"チョキ"を出したと表示
+        self.label_result.text = @"あいこで・・・";  // ジャンケン結果を表示
+        
+        // "グー", "パー"ボタンを表示
         self.button_rock.hidden = NO;
         self.button_paper.hidden = NO;
+    
     } else if (janken == 2) {
-        self.label_enemy.text = @"パー";
-        self.label_result.text = @"あなたの勝ち";
         
-        self.button_scissors.enabled = NO;
-        self.button_again.hidden = NO;
+        // COMが"パー"を選択した場合
+        self.label_enemy.text = @"パー";  // COMが"パー"を出したと表示
+        self.label_result.text = @"あなたの勝ち";  // ジャンケン結果を表示
+        
+        self.button_scissors.enabled = NO;  // "チョキ"ボタンを使用不可に
+        self.button_again.hidden = NO;  // "もう一度"ボタンを表示
     }
 }
 
+// プレイヤーが"パー"ボタンを選択した場合
 - (IBAction)paper_push:(id)sender {
+    
+    // メッセージラベルを表示
     self.label_msg.text = @"ぽん!";
     
+    // "グー", "チョキ"ボタンを隠す
     self.button_rock.hidden = YES;
     self.button_scissors.hidden = YES;
     
+    // COMのジャンケンの手を決定
     srand(time(nil));
     NSInteger janken;
     janken = rand() % 3;
     
     if (janken == 0) {
-        self.label_enemy.text = @"グー";
-        self.label_result.text = @"あなたの勝ち";
         
-        self.button_paper.enabled = NO;
-        self.button_again.hidden = NO;
+        // COMが"グー"を選択した場合
+        self.label_enemy.text = @"グー";  // COMが"グー"を出したと表示
+        self.label_result.text = @"あなたの勝ち";  // ジャンケン結果を表示
+        
+        self.button_paper.enabled = NO;  // "パー"ボタンを使用不可に
+        self.button_again.hidden = NO;  // "もう一度"ボタンを表示
+    
     } else if (janken == 1) {
-        self.label_enemy.text = @"チョキ";
-        self.label_result.text = @"あなたの負け";
         
-        self.button_paper.enabled = NO;
-        self.button_again.hidden = NO;
+        // COMが"チョキ"を選択した場合
+        self.label_enemy.text = @"チョキ";  // COMが"チョキ"を出したと表示
+        self.label_result.text = @"あなたの負け";  // ジャンケン結果を表示
+        
+        self.button_paper.enabled = NO;  // "パー"ボタンを使用不可に
+        self.button_again.hidden = NO;  // "もう一度"ボタンを表示
+
     } else if (janken == 2) {
-        self.label_enemy.text = @"パー";
-        self.label_result.text = @"あいこで・・・";
         
+        // COMが"パー"を選択した場合
+        self.label_enemy.text = @"パー";  // COMが"パー"を出したと表示
+        self.label_result.text = @"あいこで・・・";  // ジャンケン結果を表示
+        
+        // "グー", "チョキ"ボタンを表示
         self.button_rock.hidden = NO;
         self.button_scissors.hidden = NO;
     }
@@ -135,15 +177,19 @@
 - (IBAction)again_push:(id)sender {
     self.label_msg.text = @"じゃんけん・・・";
     
+    // "グー", "チョキ", "パー"ボタンを表示
     self.button_rock.hidden = NO;
     self.button_scissors.hidden = NO;
     self.button_paper.hidden = NO;
-    self.button_again.hidden = YES;
     
+    self.button_again.hidden = YES;  // "もう一度"ボタンを隠す
+
+    // "グー", "チョキ", "パー"ボタンを使用可に
     self.button_rock.enabled = YES;
     self.button_scissors.enabled = YES;
     self.button_paper.enabled = YES;
     
+    // ラベルに何も表示しない
     self.label_enemy.text = @"";
     self.label_result.text = @"";
 }
