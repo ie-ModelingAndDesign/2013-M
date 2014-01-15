be58035f96d9bc997388642091749d6c89d0619b
e125719 <e125719@e125719.st.ie.u-ryukyu.ac.jp>
2014-01-15 14:08:02 +0900

    PlayerInfoViewController 作成

---
+++ RPS3/RPS3/SessionHelper.h
@@ -0,0 +1,13 @@
+//
+//  SessionHelper.h
+//  RPS3
+//
+//  Created by e125719 on 2014/01/15.
+//  Copyright (c) 2014年 Yoshida Kannyuu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SessionHelper : NSObject
+
+@end
---
+++ RPS3/RPS3/SessionHelper.m
@@ -0,0 +1,13 @@
+//
+//  SessionHelper.m
+//  RPS3
+//
+//  Created by e125719 on 2014/01/15.
+//  Copyright (c) 2014年 Yoshida Kannyuu. All rights reserved.
+//
+
+#import "SessionHelper.h"
+
+@implementation SessionHelper
+
+@end
