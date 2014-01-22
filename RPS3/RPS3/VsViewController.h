b8ab5d84d8b69db09b7f30bf40fe02fcb104a6ad
e125719 <e125719@e125719.st.ie.u-ryukyu.ac.jp>
2014-01-22 15:55:25 +0900

    通信対戦(仮)

---
+++ RPS3/RPS3/VsViewController.h
@@ -0,0 +1,27 @@
+//
+//  VsViewController.h
+//  RPS3
+//
+//  Created by e125719 on 2014/01/22.
+//  Copyright (c) 2014年 Yoshida Kannyuu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VsViewController : UIViewController
+
+@property (weak, nonatomic) IBOutlet UILabel *label_msg;
+@property (weak, nonatomic) IBOutlet UILabel *label_enemy;
+@property (weak, nonatomic) IBOutlet UILabel *label_result;
+
+@property (weak, nonatomic) IBOutlet UIButton *button_rock;
+@property (weak, nonatomic) IBOutlet UIButton *button_scissors;
+@property (weak, nonatomic) IBOutlet UIButton *button_paper;
+@property (weak, nonatomic) IBOutlet UIButton *button_again;
+
+- (IBAction)rock_push:(id)sender;
+- (IBAction)scissors_push:(id)sender;
+- (IBAction)paper_push:(id)sender;
+- (IBAction)again_push:(id)sender;
+
+@end
